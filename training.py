f0e54d0388ff8da65a8f43f3c30f543d70732fb1
Vaishnavi <svbachiraju@gmail.com>
2026-05-27 19:23:41 -0400

    adding willow's code locally

--- training.py
+++ training.py
@@ -1,4 +1,17 @@
-import os
 from ultralytics import YOLO
 
 model = YOLO("yolov8s.pt")
+
+results=model.train(
+    data="Users/svbachiraju/Documents/Closet-Backend/outfit-generator/dataset",
+    epochs=100, #number of trainign round/numbe rof times model looks at each image. higher numbers might lead ot overfitting
+    imgsz=600, #all images aere sized to 600*600
+    batch=16, #set to -1 for mdoel to find largest batch size model cna handle withotu crashing
+    device=0, #computer uses GPU for trianing claultions, instead of CPU or aother
+    project="outfit-generator", 
+    name="training_performance"
+)
+
+
+
+
