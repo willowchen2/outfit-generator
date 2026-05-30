ad6ad370306cb24814050db5c97cd677d825018a
Vaishnavi <svbachiraju@gmail.com>
2026-05-30 11:11:22 -0400

    Cleaned up files

--- training.py
+++ training.py
@@ -4,10 +4,10 @@ model = YOLO("yolov8s.pt")
 
 results=model.train(
     data="Users/svbachiraju/Documents/Closet-Backend/outfit-generator/dataset",
-    epochs=100, #number of trainign round/numbe rof times model looks at each image. higher numbers might lead ot overfitting
+    epochs=10, #number of trainign round/numbe rof times model looks at each image. higher numbers might lead ot overfitting
     imgsz=600, #all images aere sized to 600*600
     batch=16, #set to -1 for mdoel to find largest batch size model cna handle withotu crashing
-    device=0, #computer uses GPU for trianing claultions, instead of CPU or aother
+    device='cpu', #computer uses GPU for trianing claultions, instead of CPU or aother
     project="outfit-generator", 
     name="training_performance"
 )
